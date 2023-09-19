48f0e8da4832ac1b7a8a7ba118eaa5f63faf0cab
Romeo509 <romeoameku503@gmail.com>
2023-09-19 01:57:08 +0100

    update

--- _printf.c
+++ _printf.c
@@ -1,29 +1,75 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: the format
- *
- * Return: the total count of the printed characters
- */
-
+* _printf - Custom implementation of the printf function
+* @format: The format specifier
+* Return: The number of characters printed (excluding the null byte)
+*/
 int _printf(const char *format, ...)
 {
-	int printed_char;
-	conver_t func_list[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"%", print_percent}
-	};
-	va_list arg_list;
-
-	if (format == NULL)
-		return (-1);
-	va_start(arg_list, format);
-
-	printed_char = parser(format, func_list, arg_list);
-
-	va_end(arg_list);
-
-	return (printed_char);
+int printed_chars = 0;
+va_list arg_list;
+va_start(arg_list, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+printed_chars = custom_selector(format, arg_list, printed_chars);
+format++;
 }
+else
+{
+_putchar(*format);
+printed_chars++;
+format++;
+}
+}
+va_end(arg_list);
+return (printed_chars);
+}
+/**
+* custom_selector - Process and print a custom format specifier
+* @format: The format specifier
+* @args: The argument list
+* @count: The current count of printed characters
+* Return: The updated count of printed characters
+*/
+int custom_selector(const char *format, va_list args, int count)
+{
+if (*format == 'c')
+{
+char ch = va_arg(args, int);
+_putchar(ch);
+count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+count += custom_puts(str);
+}
+else if (*format == '%')
+{
+_putchar('%');
+count++;
+}
+return (count);
+}
+/**
+* custom_puts - Print a custom string and return the number of characters printed
+* @str: The string to print
+* Return: The number of characters printed
+*/
+int custom_puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+return (i);
+}
+
--- _putchar.c
+++ _putchar.c
@@ -1,76 +1,13 @@
 #include "main.h"
 
 /**
-* _printf - Custom implementation of the printf function
-* @format: The format specifier
-* Return: The number of characters printed (excluding the null byte)
-*/
-int _printf(const char *format, ...)
+ * _putchar - write the character c to stdout
+ * @c: the character
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
 {
-int printed_chars = 0;
-va_list arg_list;
-va_start(arg_list, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-printed_chars = custom_selector(format, arg_list, printed_chars);
-format++;
+	return (write(1, &c, 1));
 }
-else
-{
-_putchar(*format);
-printed_chars++;
-format++;
-}
-}
-va_end(arg_list);
-return (printed_chars);
-}
-
-/**
-* custom_selector - Process and print a custom format specifier
-* @format: The format specifier
-* @args: The argument list
-* @count: The current count of printed characters
-* Return: The updated count of printed characters
-*/
-int custom_selector(const char *format, va_list args, int count)
-{
-if (*format == 'c')
-{
-char ch = va_arg(args, int);
-_putchar(ch);
-count++;
-}
-else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-count += custom_puts(str);
-}
-else if (*format == '%')
-{
-_putchar('%');
-count++;
-}
-return (count);
-}
-/**
-* custom_puts - Print a custom string and return the number of characters printed
-* @str: The string to print
-* Return: The number of characters printed
-*/
-int custom_puts(char *str)
-{
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-return (i);
-}
-
