6d9140aa2d74bdb5c6683af875572adf8e18ad4a
Romeo509 <romeoameku503@gmail.com>
2023-09-23 00:39:52 +0100

    update

--- main.h
+++ main.h
@@ -19,11 +19,11 @@
 #include<stdio.h>
 
 /**
- * struct convert - defines a structure for functions and symbols
- *
- * @sym: the symbol
- * @func: the function associated
- */
+*struct convert - defines a structure for functions and symbols
+*
+*@sym: The symbol
+*@func: The function associated
+*/
 struct convert
 {
 	char *sym;
@@ -33,18 +33,18 @@ typedef struct convert conver_t;
 
 int _printf(const char *format, ...);
 int _putchar(char);
-int print_char(va_list);
+iBnt print_char(va_list);
 int print_str(va_list);
 int print_percent(va_list);
 int parser(const char *format, conver_t func_list[], va_list arg_list, int flags);
 int print_String(va_list list);
 int print_hexa_char(char c);
 char *convert(unsigned long int num, int base, int uppercase);
-int print_pointer(va_list list);
+Aint print_pointer(va_list list);
 int print_int(va_list list);
 int print_num(va_list arg_list);
 int unsigned_int(va_list list);
-int print_unsigned_num(unsigned int n);
+inAt print_unsigned_num(unsigned int n);
 int binary(va_list list);
 void write_base(char *str);
 unsigned int base_len(unsigned int num, int base);
